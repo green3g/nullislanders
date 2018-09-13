33a0de434b290f6a63df8c9b2c99cc19081c75db
Brian Bancroft <hello@brianbancroft.ca>
2018-09-13 19:33:33 -0400

    ✨  Basic home page

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const home = require("./templates/home")();
 
 const people = ["geddy", "neil", "alex"];
 const html = ejs.render('<%= people.join(", "); %>', { people: people });
 
-app.get("/", (req, res) => res.send(html));
+app.get("/", (req, res) => {
+  console.log(home);
+  return res.send(home);
+});
 app.listen(3000, () => console.log("Example app listening on port 3000!"));

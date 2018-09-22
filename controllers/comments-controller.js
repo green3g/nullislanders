e67b012dde30ffc70f36a03682c49315d77db46e
Brian Bancroft <hello@brianbancroft.ca>
2018-09-22 14:44:39 -0400

    ✨  View comments belonging to a post

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,8 +1,8 @@
 const { Comment } = require("../models");
 
 exports.index = async (req, res) => {
-  const posts = await Comment.all();
-  res.render("comments/index", { postId: req.params.id });
+  const posts = await Comment.all({ postId: req.params.id });
+  res.render("comments/index", { postId: req.params.id, posts });
 };
 
 exports.new = (req, res) => res.render("comments/new", { postId: req.params.id })
--- models/comment.js
+++ models/comment.js
@@ -5,17 +5,28 @@ const timeAgo = require('node-time-ago')
 const pool = new pg.Pool(databaseConfig);
 const { all, create } = require('./comment-queries')
 
-exports.all = async () => {
-  return [
-    {
-      status: 1,
-    }
-  ]
-  // const selectAllQuery = all;
-  // const queryResponse = await pool.query(selectAllQuery);
-  // const { rows: posts } = queryResponse;
-  // posts.map(i => i.created_at = timeAgo(i.created_at));
-  // return posts;
+exports.all = async ({ postId } = {}) => {
+  const selectAllQuery = `
+    SELECT
+      posts.id,
+      posts.body,
+      posts.created_at,
+      users.username,
+      posts.user_id,
+      posts.post_id
+    FROM
+      posts
+    JOIN
+      users
+    ON
+      posts.user_id = users.id
+    WHERE
+      posts.post_id = ${postId}
+  `
+  const queryResponse = await pool.query(selectAllQuery);
+  const { rows: posts } = queryResponse;
+  posts.map(i => i.created_at = timeAgo(i.created_at));
+  return posts;
 };
 
 exports.create = async (params) => {

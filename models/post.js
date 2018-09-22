4c507349f883b95152bceb30a2ec76fdb4c20bb2
Brian Bancroft <hello@brianbancroft.ca>
2018-09-22 09:21:10 -0400

    ✨  ➕  List when posts were submitted
    Uses node-time-ago v1.0.0

--- models/post.js
+++ models/post.js
@@ -1,18 +1,22 @@
 const pg = require("pg");
 const { databaseConfig } = require("../helpers");
+const timeAgo = require('node-time-ago')
+
 const pool = new pg.Pool(databaseConfig);
 const { all, create } = require('./post-queries')
 
 exports.all = async () => {
   const selectAllQuery = all;
   const queryResponse = await pool.query(selectAllQuery);
-  const { rows: records } = queryResponse;
-  return records;
+  const { rows: posts } = queryResponse;
+  posts.map(i => i.created_at = timeAgo(i.created_at));
+  return posts;
 };
 
 exports.create = async (params) => {
   const createNewQuery = create(params);
   const queryResponse = await pool.query(createNewQuery);
-  const { rows: record } = queryResponse;
-  return record;
+  const { rows: post } = queryResponse;
+
+  return post;
 };

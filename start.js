0e1689434becfdc5c968b4d7861303f34339af33
Brian Bancroft <hello@brianbancroft.ca>
2018-09-30 13:17:40 -0400

    🔧  Default to port 3000

--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 // Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split(".").map(parseFloat);
+const [major, minor,] = process.versions.node.split('.').map(parseFloat)
 if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log("🛑  You're on an older version of node  ");
-  process.exit();
+  console.log('🛑  You\'re on an older version of node  ')
+  process.exit()
 }
 
-require("dotenv").config({ path: ".env" });
+require('dotenv').config({ path: '.env', })
 
-const app = require("./app");
-app.set("port", process.env.PORT || 7777);
-const server = app.listen(app.get("port"), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+const app = require('./app')
+app.set('port', process.env.PORT || 3000)
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`)
+})
